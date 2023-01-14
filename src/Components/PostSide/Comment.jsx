2224caaf1323d45488bbc5acf56bb796405f6d87
piyush-agrawal6 <agrawaljoy1@gmail.com>
2023-01-14 09:34:59 +0530

    comment section added

--- src/Components/PostSide/Comment.jsx
+++ src/Components/PostSide/Comment.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Comment.css";
 const Comment = () => {
-  return <div>Comment</div>;
+  return (
+    <div className="comments">
+      <img src="" alt="" />
+      <div className="commentInfo">
+        <span>Commenter</span>
+        <p>Commenter's comment</p>
+      </div>
+    </div>
+  );
 };
 
 export default Comment;
